f435d7bce0260b77ef000a58c83f9727ddc21ea7
Angelos Evripiotis <jevripiotis@bloomberg.net>
2013-12-13 15:32:32 +0000

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! doc/design: proposal dependent branches

--- doc/design/make_branches.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-import subprocess
-commit_list = subprocess.check_output(['git', 'rev-list', 'master..'])
-branch = 'master'
-for commit in reversed(commit_list.splitlines()):
-    print subprocess.check_output(['git', 'log', '-n', '1', commit])
-    description = raw_input('one word description: ')
-    branch = "r/{}/{}".format(branch, description)
-    print subprocess.check_output(['git', 'branch', branch, commit])
