00236f7968fb42a40fee9456df145d1bb2cc7760
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-07-03 18:26:38 +0100

    Arcyd: refactor, mv repo config parsing to 'start'
    Make it possible to do all disk access to repository config files for
    'arcyd start' before doing abdi_processrepos.process().
    
    This makes it possible to get a lock on the repository config files
    at the top-level of the program and release it before descending into
    the arcyd daemon details.
    
    Test Plan:
    $ ./precommit.sh

--- py/abd/abdcmd_start.py
+++ py/abd/abdcmd_start.py
@@ -22,6 +22,7 @@ import phlsys_pid
 import phlsys_signal
 
 import abdi_processrepos
+import abdi_repoargs
 import abdt_fs
 
 
@@ -71,9 +72,13 @@ def process(args):
         if args.no_loop:
             params.append('--no-loop')
 
-    abdi_processrepos.setupParser(parser)
-    args = parser.parse_args(params)
-    abdi_processrepos.process(args, fs.repo_config_path_list())
+        repo_configs = abdi_repoargs.parse_config_file_list(
+            fs.repo_config_path_list())
+
+        abdi_processrepos.setupParser(parser)
+        args = parser.parse_args(params)
+
+    abdi_processrepos.process(args, repo_configs)
 
 
 # -----------------------------------------------------------------------------
