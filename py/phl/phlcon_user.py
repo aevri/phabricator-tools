4e162bad1cec3224cc5d49894adab8c5a42c1ec1
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-09-23 18:00:24 +0100

    Revert "WIP: add phlcon_user.get_username_phid"
    This reverts commit c9fabe32cc007422789a7439683d37aaf81ac92d.

--- py/phl/phlcon_user.py
+++ py/phl/phlcon_user.py
@@ -81,12 +81,6 @@ class UnknownPhid(Error):
         super(UnknownPhid, self).__init__(phid)
 
 
-class UnknownEmail(Error):
-
-    def __init__(self, email):
-        super(UnknownEmail, self).__init__(email)
-
-
 class UsernamePhidCache(object):
 
     """Efficiently retrieve the PHID for specified usernames."""
@@ -96,7 +90,6 @@ class UsernamePhidCache(object):
         super(UsernamePhidCache, self).__init__()
         self._username_to_phid = {}
         self._phid_to_username = {}
-        self._email_to_username_phid = {}
         self._hinted_usernames = set()
         self._conduit = conduit
 
@@ -155,21 +148,6 @@ class UsernamePhidCache(object):
             self._hinted_usernames -= set(username_to_phid.iterkeys())
 
         return self._phid_to_username[phid]
-
-    def get_username_phid(self, email):
-        """Return the username and phid for the specified 'email'."""
-        if email not in self._email_to_username_phid:
-
-            user = query_user_from_email(self._conduit, email)
-            if user is None:
-                raise UnknownEmail(email)
-
-            self._email_to_username_phid[email] = (user.userName, user.phid)
-            self._phid_to_username[user.phid] = user.userName
-            self._username_to_phid[user.userName] = user.phid
-            self._hinted_usernames.discard(user.userName)
-
-        return self._email_to_username_phid[email]
 
 
 def is_no_such_error(e):
