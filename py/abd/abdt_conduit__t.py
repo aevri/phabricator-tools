aac0fb2c7c486993a321f57ce6340afc974f43ed
Angelos Evripiotis <jevripiotis@bloomberg.net>
2014-12-11 23:43:14 +0000

    Arcyd: refactor, remove refresh_cache_on_cycle()
    Remove an imp detail from the interface of abdt_conduit. It's purpose is
    to make abdi_processrepo simple, conduit refresh isn't a part of that
    story.
    
    Refresh the cache object directly instead.
    
    Test plan:
    $ ./precommit.sh

--- py/abd/abdt_conduit__t.py
+++ py/abd/abdt_conduit__t.py
@@ -74,12 +74,13 @@ class Test(unittest.TestCase):
             self.test_data.TEST_URI,
             self.test_data.PHAB.user,
             self.test_data.PHAB.certificate)
+        self._review_state_cache = phlcon_reviewstatecache.make_from_conduit(
+            self.sys_conduit)
         self.conduit = abdt_conduit.Conduit(
-            self.sys_conduit,
-            phlcon_reviewstatecache.make_from_conduit(self.sys_conduit))
+            self.sys_conduit, self._review_state_cache)
 
     def _invalidate_cache(self):
-        self.conduit.refresh_cache_on_cycle()
+        self._review_state_cache.refresh_active_reviews()
 
     def tearDown(self):
         pass
