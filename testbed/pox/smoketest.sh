d9d74bd0535f07fa6f84f16b450fd4d16428e48c
Angelos Evripiotis <jevripiotis@bloomberg.net>
2015-07-10 13:30:09 +0100

    WIP

--- testbed/pox/smoketest.sh
+++ testbed/pox/smoketest.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 ../../proto/arcyd-tester --phab-uri http://127.0.0.1:8090
-../../bin/arcyon query --uri http://127.0.0.1:8081 --user me --cert squirrel
+../../bin/arcyon query --uri http://127.0.0.1:8091 --user me --cert squirrel
