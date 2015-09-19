e0c068b9b83c09b474330295024145e18d14f52f
Angelos Evripiotis <angelos.evripiotis@gmail.com>
2015-09-19 21:54:53 +0100

    WIP: fix exercise_arcyon

--- testbed/arcyon/exercise_arcyon.sh
+++ testbed/arcyon/exercise_arcyon.sh
@@ -18,49 +18,56 @@ $arcyon update-revision -h
 $arcyon task-create -h
 $arcyon task-query -h
 
-id="$($arcyon create-revision -t title -p plan --summary ssss -f diff1 --format-id)"
-$arcyon get-diff -r $id --ls
-$arcyon update-revision $id update -f diff2
-$arcyon get-diff -r $id --ls
+cert=xnh5tpatpfh4pff4tpnvdv74mh74zkmsualo4l6mx7bb262zqr55vcachxgz7ru3lrv\
+afgzquzl3geyjxw426ujcyqdi2t4ktiv7gmrtlnc3hsy2eqsmhvgifn2vah2uidj6u6hhhxo2j3y2w\
+6lcsehs2le4msd5xsn4f333udwvj6aowokq5l2llvfsl3efcucraawtvzw462q2sxmryg5y5rpicdk\
+3lyr3uvot7fxrotwpi3ty2b2sa2kvlpf
 
-$arcyon query --format-type ids | grep $id
-$arcyon query --ids $id --format-string '$summary' | grep ssss
-$arcyon query --format-type ids --order created | grep $id
-$arcyon query --format-type ids --order modified | grep $id
+creds="--uri http://127.0.0.1 --user phab --cert $cert"
 
-diffid="$($arcyon raw-diff diff1)"
-diffid2="$($arcyon raw-diff diff2)"
-$arcyon get-diff -d $diffid --ls
-$arcyon get-diff -d $diffid2 --ls
-id2="$($arcyon create-revision -t title2 -p plan --diff-id $diffid --format-id)"
-id3=$($arcyon update-revision $id2 update --diff-id $diffid2 --format-id)
-$arcyon update-revision $id2 update --diff-id $diffid2 --format-url
-$arcyon update-revision $id2 update --diff-id $diffid2 --format-url --ccs phab --reviewers bob
+id="$($arcyon create-revision -t title -p plan --summary ssss -f diff1 --format-id $creds)"
+$arcyon get-diff -r $id --ls $creds
+$arcyon update-revision $id update -f diff2 $creds
+$arcyon get-diff -r $id --ls $creds
+
+$arcyon query --format-type ids $creds | grep $id
+$arcyon query --ids $id --format-string '$summary' $creds | grep ssss
+$arcyon query --format-type ids --order created $creds | grep $id
+$arcyon query --format-type ids --order modified $creds | grep $id
+
+diffid="$($arcyon raw-diff diff1 $creds)"
+diffid2="$($arcyon raw-diff diff2 $creds)"
+$arcyon get-diff -d $diffid --ls $creds
+$arcyon get-diff -d $diffid2 --ls $creds
+id2="$($arcyon create-revision -t title2 -p plan --diff-id $diffid --format-id $creds)"
+id3=$($arcyon update-revision $id2 update --diff-id $diffid2 --format-id $creds)
+$arcyon update-revision $id2 update --diff-id $diffid2 --format-url $creds
+$arcyon update-revision $id2 update --diff-id $diffid2 --format-url --ccs phab --reviewers bob $creds
 
 if [ "$id2" != "$id3" ]; then
     false
 fi
 
-$arcyon query --format-type ids | grep $id2
+$arcyon query --format-type ids $creds | grep $id2
 
-$arcyon comment $id2 -m 'hello there!'
-$arcyon comment-inline $id2 --start-line 51 --end-line-offset 0 --filepath 'bin/arcyon' -m 'inline comment!'
-$arcyon comment-inline $id2 --start-line 51 --end-line-offset 0 --filepath 'bin/arcyon' -m 'old-side inline comment!' --left-side
-$arcyon comment $id2 --attach-inlines
+$arcyon comment $id2 -m 'hello there!' $creds
+$arcyon comment-inline $id2 --start-line 51 --end-line-offset 0 --filepath 'bin/arcyon' -m 'inline comment!' $creds
+$arcyon comment-inline $id2 --start-line 51 --end-line-offset 0 --filepath 'bin/arcyon' -m 'old-side inline comment!' --left-side $creds
+$arcyon comment $id2 --attach-inlines $creds
 
-taskid=$($arcyon task-create 'exercise task-create' -d 'description' -p wish -o alice --ccs phab bob --format-id)
-$arcyon task-query
-taskid2=$($arcyon task-query --max-results 1 --format-ids)
+taskid=$($arcyon task-create 'exercise task-create' -d 'description' -p wish -o alice --ccs phab bob --format-id $creds)
+$arcyon task-query $creds
+taskid2=$($arcyon task-query --max-results 1 --format-ids $creds)
 
 if [ "$taskid" != "$taskid2" ]; then
     false
 fi
 
-$arcyon task-create 'exercise task-create again'
-$arcyon task-update $taskid -m 'just a comment'
-$arcyon task-update $taskid -t 'exercise task-update' -d 'new description' -p low -o bob --ccs phab alice -m 'updated loads'
+$arcyon task-create 'exercise task-create again' $creds
+$arcyon task-update $taskid -m 'just a comment' $creds
+$arcyon task-update $taskid -t 'exercise task-update' -d 'new description' -p low -o bob --ccs phab alice -m 'updated loads' $creds
 
-$arcyon paste "test paste" -f diff1
+$arcyon paste "test paste" -f diff1 $creds
 # -----------------------------------------------------------------------------
 # Copyright (C) 2013-2015 Bloomberg Finance L.P.
 #
