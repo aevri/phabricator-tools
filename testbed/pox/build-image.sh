b1c1fdbd6af83b8f414645404461cc1a633532c1
Angelos Evripiotis <jevripiotis@bloomberg.net>
2016-01-12 13:34:26 +0000

    WIP: build-image support Mac

--- testbed/pox/build-image.sh
+++ testbed/pox/build-image.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 THIS_DIR=$(cd $(dirname $0); pwd -P)
 echo ${THIS_DIR}
-BUILD_DIR=$(mktemp -d)
+BUILD_DIR=$(mktemp -d /tmp/temp.XXXXX)
 
 pushd ${BUILD_DIR}
 cp -R "${THIS_DIR}/../.." ${BUILD_DIR}
